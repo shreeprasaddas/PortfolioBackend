6ed67d794fc7f903728cdf0a0743766c23713cdb
ShreePrasad Das <shreepsd2@gmail.com>
2025-06-26 19:44:37 +0545

    update mongodb uri

--- databaseConnection/database_connection.js
+++ databaseConnection/database_connection.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
-// const uri = "mongodb+srv://shreepsd:shreepsd%402227@cluster0.9jzuy.mongodb.net/portfolio";
-const uri2 = "mongodb://localhost:27017/portfolio";
+const uri = process.env.MONGODB_URI || "mongodb://localhost:27017/portfolio";
+// const uri2 = "mongodb://localhost:27017/portfolio";
 
 const DataBase= mongoose;
-await mongoose.connect(uri2).then(()=>{
+await mongoose.connect(uri).then(()=>{
     console.log("database connection successfully");
 })
 .catch((error)=>{

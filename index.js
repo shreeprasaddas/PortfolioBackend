f7864fa26d09adb891d790e29b1011d0c6152e69
ShreePrasad Das <shreepsd2@gmail.com>
2026-04-26 15:20:26 +0545

    fix: add new frontend origin https://frontend-ten-alpha-64.vercel.app

--- index.js
+++ index.js
@@ -34,7 +34,8 @@ const app = express();
 const allowedOrigins = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
-    'https://frontend-ten-alpha-64.vercel.app/',
+    'https://frontend-ten-alpha-64.vercel.app',
+    'https://portfolio-frontend-react-mauve.vercel.app',
     ...(process.env.FRONTEND_URL ? process.env.FRONTEND_URL.split(',').map(u => u.trim()) : [])
 ];
 
